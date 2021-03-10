85c9acbf8350d5baf4f6571e6328187e72438043
diuguide <everett.diuguid@gmail.com>
2021-03-10 11:39:59 -0500

    implemented react and node-sass, header and background image placed

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './scss/App.scss';
+import HeaderDiv from "./components/Header";
+
+
+function App() {
+  return (
+    <HeaderDiv />
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,25 @@
+
+const HeaderDiv = () => {
+  return (
+    <header>
+      <nav>
+        <h1>Mars Perserverance Rover</h1>
+        <ul>
+          <li>
+            <a href="#updates">Updates</a>
+          </li>
+          <li>
+            <a target="_blank" href="https://mars.nasa.gov/mars2020/">
+              Perserverance NASA Website
+            </a>
+          </li>
+          <li>
+            <a href="#images">Images</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default HeaderDiv;
