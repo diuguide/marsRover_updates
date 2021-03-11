7acdfccdf98bba34164c1cab321d5a1e030ab081
diuguide <everett.diuguid@gmail.com>
2021-03-10 22:45:44 -0500

    added new post, added gh-pages

--- client/src/textData/posts.js
+++ client/src/textData/posts.js
@@ -1,24 +1,32 @@
 const postData = {
   posts: [
     {
+      title: "Message in a Parachute",
+      text1:
+        "Mars Decoder Ring: This annotated image was taken during the landing of the Perseverance rover on Feb. 18, 2021. Using binary code, two messages were encoded in the parachute: DARE MIGHTY THINGS and the GPS coordinates of an area by JPL’s visitor center. Credit: NASA/JPL-Caltech.",
+      source:
+        "https://mars.nasa.gov/news/8875/testing-proves-its-worth-with-successful-mars-parachute-deployment/",
+      img:
+        "https://www.dropbox.com/s/2f9hfhbytfej5lt/parachuteMessage.jpg?raw=1",
+      alt: "secret message decoded within mars parachute",
+    },
+    {
       title: "Ingenuity Reports for Duty",
       text1:
         "Mission controllers at NASA’s Jet Propulsion Laboratory in Southern California have received the first status report from the Ingenuity Mars Helicopter, which landed Feb. 18, 2021, at Jezero Crater attached to the belly of the agency’s Mars 2020 Perseverance rover. The downlink, which arrived at 3:30 p.m. PST (6:30 p.m. EST) via a connection through the Mars Reconnaissance Orbiter, indicates that both the helicopter, which will remain attached to the rover for 30 to 60 days, and its base station (an electrical box on the rover that stores and routes communications between the rotorcraft and Earth) are operating as expected.",
-     
       source:
         "https://mars.nasa.gov/news/8867/nasas-mars-helicopter-reports-in/",
-          img: "https://www.dropbox.com/s/v0vt43x80whr411/selfie.png?raw=1",
+      img: "https://www.dropbox.com/s/v0vt43x80whr411/selfie.png?raw=1",
       alt: "rover selfie",
     },
     {
       title: "Touchdown!",
       text1:
         "The largest, most advanced rover NASA has sent to another world touched down on Mars Thursday, after a 203-day journey traversing 293 million miles (472 million kilometers). Confirmation of the successful touchdown was announced in mission control at NASA’s Jet Propulsion Laboratory in Southern California at 3:55 p.m. EST (12:55 p.m. PST).",
-     
       source:
         "https://mars.nasa.gov/news/8865/touchdown-nasas-mars-perseverance-rover-safely-lands-on-red-planet/",
-        img: "https://www.dropbox.com/s/8swzs8y2rppnot2/roverDescent.png?raw=1",
-      alt : "rover descending",
+      img: "https://www.dropbox.com/s/8swzs8y2rppnot2/roverDescent.png?raw=1",
+      alt: "rover descending",
     },
   ],
 };
