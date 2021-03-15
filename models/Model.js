6112a31386d2da60f3df85fc48ee9b64fc298f24
diuguide <everett.diuguid@gmail.com>
2021-03-14 22:30:35 -0400

    added to model

--- models/Model.js
+++ models/Model.js
@@ -2,14 +2,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const EmailList = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        trim: true,
-        required: "Please provide an email if you would like to join our email list."
-    }
-})
-const Email = mongoose.model('email', EmailList);
+const EmailSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    unique: true,
+    trim: true,
+  },
+});
+const Email = mongoose.model('email', EmailSchema);
 
 module.exports = Email;
--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 //Model
-const EmailList = require("../models/Model");
+const Email = require("../models/Model");
 
 //GET
 router.get("/", (req, res) => {
-    res.send("Succesful! GET");
+    Email.find({})
+      .then((data) => res.json(data))
+      .catch((err) => console.log("Error GET: ", err));
 });
 
 //POST
 router.post("/add", (req, res) => {
-    res.send("Succesful! PUT");
+    const { email } = req.body;
+    const newEmail = new Email({ email });
+    newEmail.save();
+    res.json(newEmail);
+
+    // if (!email) return res.sendStatus(400).json({ msg: "Please Enter an email" });
+    // EmailList.findOne({ email }).then((email) => {
+    //     if (email) return res.sendStatus(400).json({ msg: "Email has already been added to our list" });
+    //     const newEmail = new EmailList({
+    //         email
+    //     }).
+    //         res.json(newEmail);
+
+    // })
+
 })
 
 module.exports = router;
